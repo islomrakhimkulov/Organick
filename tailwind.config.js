7d65018569802b55b160da677d4eefd3e8d3b4d2
https://github.com/islomrakhimkulov <islomraximqulov448@gmail.com>
2022-05-22 18:18:11 +0500

    created

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        container: {
+            padding: "2rem",
+        },
+        fontFamily: {
+            roboto: ['"Roboto"', '"sans-serif"'],
+            yellowtail: ['"Yellowtail"', '"cursive"'],
+        },
+        backgroundImage: {
+            "header-bg": "url('./images/header-bg.png')",
+            "about-bg": "url('./images/about-bg.png')",
+            "testimonial-bg": "url('./images/testimonial-bg.png')",
+            "friendly-bg": "url('./images/friendly-bg.png')",
+            "newsletter-bg": "url('./images/newslatter-bg.png')",
+        },
+        fontSize: {
+            sm: ["18px"],
+        },
+        extend: {
+            colors: {
+                "organ-green": "#7EB693",
+                "organ-main": "#274C5B",
+                "organ-button": "#EFD372",
+                "organ-button-in": "#335B6B",
+            },
+        },
+    },
+    plugins: [],
+};
